ca00f60fab57b0215c53cb402c06a35c4a3430b2
FSHimel <shahriarfahim067@gmail.com>
2026-05-24 02:26:08 +0600

    my transaction added

--- src/Pages/MyTransactions.jsx
+++ src/Pages/MyTransactions.jsx
@@ -1,5 +1,70 @@
+import { use, useEffect, useState } from "react";
+import AuthContext from "../Firebase/AuthContext";
+import { Link } from "react-router";
+
 const MyTransactions = () => {
-  return <div>My Transactions</div>;
+  const { user } = use(AuthContext);
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    if (user?.email) {
+      fetch(
+        `https://fineaseserver-sooty.vercel.app/transactions?email=${user.email}`,
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setTransactions(data);
+        });
+    }
+  }, [user?.email]);
+  return (
+    <div className="w-11/12 mx-auto">
+      <h1 className="text-3xl text-center mt-5 font-black">
+        <span className="text-[#785964]">My Transactions:</span>{" "}
+        {transactions.length}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-5">
+        {transactions.map((transaction) => (
+          <div
+            key={transaction._id}
+            className="border rounded-xl p-5 shadow-md bg-white"
+          >
+            <h2 className="text-xl font-semibold mb-2 capitalize">
+              {transaction.type}
+            </h2>
+
+            <p>
+              <span className="font-bold">Category:</span>{" "}
+              {transaction.category}
+            </p>
+
+            <p>
+              <span className="font-bold">Amount:</span> ${transaction.amount}
+            </p>
+
+            <p>
+              <span className="font-bold">Date:</span>{" "}
+              {new Date(transaction.date).toLocaleDateString()}
+            </p>
+
+            <div className="flex flex-wrap gap-3 mt-5">
+              {/* Update Button */}
+              <Link to={`/update/${transaction._id}`}>
+                <button className="btn btn-sm btn-warning">Update</button>
+              </Link>
+
+              {/* Delete Button */}
+              <button className="btn btn-sm btn-error">Delete</button>
+
+              {/* View Details Button */}
+              <Link to={`/transactions/${transaction._id}`}>
+                <button className="btn btn-sm btn-info">View Details</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyTransactions;
